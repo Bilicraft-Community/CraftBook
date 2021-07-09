aba2d40cea0f1c8ab39903802c836c731a3ce677
Ghost_chu <2908803755@qq.com>
2021-07-09 19:22:47 +0800

    Speed Modifiers only apply to regular minecart

--- src/main/java/com/sk89q/craftbook/mechanics/minecart/SpeedModifiers.java
+++ src/main/java/com/sk89q/craftbook/mechanics/minecart/SpeedModifiers.java
@@ -1,5 +1,6 @@
 package com.sk89q.craftbook.mechanics.minecart;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,12 +13,16 @@ import com.sk89q.util.yaml.YAMLProcessor;
 
 public class SpeedModifiers extends AbstractCraftBookMechanic {
 
+
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onVehicleCreate(VehicleCreateEvent event) {
 
         if(!EventUtil.passesFilter(event)) return;
 
         if (!(event.getVehicle() instanceof Minecart)) return;
+
+        if(onlyRegularMinecart && event.getVehicle().getType() == EntityType.MINECART) return;
 
         if (offRail > 0)
             ((Minecart) event.getVehicle()).setDerailedVelocityMod(new Vector(offRail, offRail, offRail));
@@ -27,6 +32,7 @@ public class SpeedModifiers extends AbstractCraftBookMechanic {
 
     private double maxSpeed;
     private double offRail;
+    private boolean onlyRegularMinecart;
 
     @Override
     public void loadConfiguration (YAMLProcessor config, String path) {
@@ -36,5 +42,8 @@ public class SpeedModifiers extends AbstractCraftBookMechanic {
 
         config.setComment(path + "off-rail-speed", "Sets the off-rail speed modifier of carts. 0 is none.");
         offRail = config.getDouble(path + "off-rail-speed", 0);
+
+        config.setComment(path + "only-regular-minecart", "Sets only apply regular minecart speed.");
+        onlyRegularMinecart = config.getBoolean(path + "only-regular-minecart", true);
     }
 }
