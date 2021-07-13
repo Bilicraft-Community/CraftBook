c09a427e03bfbe5579a7317faff3937d1eff392f
Ghost_chu <2908803755@qq.com>
2021-07-13 16:12:14 +0800

    如果矿车被命名，则不受Speed Modifiers影响

--- src/main/java/com/sk89q/craftbook/mechanics/minecart/SpeedModifiers.java
+++ src/main/java/com/sk89q/craftbook/mechanics/minecart/SpeedModifiers.java
@@ -1,5 +1,6 @@
 package com.sk89q.craftbook.mechanics.minecart;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Minecart;
 import org.bukkit.event.EventHandler;
@@ -23,6 +24,10 @@ public class SpeedModifiers extends AbstractCraftBookMechanic {
         if (!(event.getVehicle() instanceof Minecart)) return;
 
         if(onlyRegularMinecart && event.getVehicle().getType() != EntityType.MINECART) return;
+
+        if(StringUtils.isEmpty(event.getVehicle().getCustomName())){
+            return;
+        }
 
         if (offRail > 0)
             ((Minecart) event.getVehicle()).setDerailedVelocityMod(new Vector(offRail, offRail, offRail));
